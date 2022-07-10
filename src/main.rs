5da802d04e3f57101a9af22059a63ca81510bbb6
popgthyrd@gmail.com <popgthyrd@gmail.com>
2022-07-10 22:38:36 +0200

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+  let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
+  println!("listening started, ready to accept");
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
+
+    handle_connection(stream)
+  }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+  let mut buf = [0; 1024];
+  
+  stream.read(&mut buf).unwrap();
+
+  println!("Request: {}", String::from_utf8_lossy(&buf[..]));
+}
