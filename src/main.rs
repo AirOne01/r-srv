592b7b6d06a7109cdac5da2d0583fc9153c247b2
popgthyrd@gmail.com <popgthyrd@gmail.com>
2022-07-10 23:14:14 +0200

    feat: add extra packet debug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::fmt::Write;
 
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:9999").unwrap();
@@ -16,5 +17,14 @@ fn handle_connection(mut stream: TcpStream) {
   
   stream.read(&mut buf).unwrap();
 
-  println!("Request: {}", String::from_utf8_lossy(&buf[..]));
+  println!("Request: {}", String::from_utf8_lossy(&buf));
+  write_bytes(&buf);
+}
+
+fn write_bytes(buf: &[u8]) {
+  let mut s = String::new();
+  for &byte in buf {
+    write!(&mut s, "{:X}", byte).expect("Unable to write");
+  }
+  println!("{}\n", s);
 }
